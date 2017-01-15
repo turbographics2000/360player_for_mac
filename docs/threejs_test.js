0d0e7b0ad605eadef244bb5f7d6b8ba54ea8a87a
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-16 06:33:38 +0900

    Update threejs_test.js

--- docs/threejs_test.js
+++ docs/threejs_test.js
@@ -1,7 +1,7 @@
 var cnv = document.createElement('canvas');
 var vid = document.createElement('video');
-vid.width = 1920;
-vid.height = 960;
+vid.width = 2160;
+vid.height = 1080;
 cnv.crossOrigin = "anonymous";
 vid.crossOrigin = "anonymous";
 var ctx = null;
