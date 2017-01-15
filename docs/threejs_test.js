46389a6a12860529f59f9cb5ef01f0cacc2e61dc
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-16 05:59:23 +0900

    Update threejs_test.js

--- docs/threejs_test.js
+++ docs/threejs_test.js
@@ -30,7 +30,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, 640 / 480, 0.1, 10000);
 var controls = new THREE.VRControls(camera);
 var geometry = new THREE.SphereBufferGeometry(100, 64, 64);
-var texture = new THREE.Texture(vid);
+var texture = new THREE.Texture(cnv);
 var material = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide});
 var mesh = new THREE.Mesh(geometry, material);
 scene.add(camera);
