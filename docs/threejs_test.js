d2a9e255c2246c0a096d22302f20b0e5385261aa
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-16 11:56:39 +0900

    Update threejs_test.js

--- docs/threejs_test.js
+++ docs/threejs_test.js
@@ -27,7 +27,7 @@ if(window.chrome) {
 } else {
   //vid.src = 'https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8';
   //vid.src = 'https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560_video_1080_5000000.m3u8';
-  vid.src = 'output_2pass_1Mbps_placebo.mp4';
+  vid.src = 'output.mp4';
   vid.onloadedmetadata = function() {
     ctx = cnv.getContext('2d');
     vid.play();
