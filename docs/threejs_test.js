9463a1f827ae1c53ecb4fe34b8b3b760186b3adc
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-16 11:31:19 +0900

    Update threejs_test.js

--- docs/threejs_test.js
+++ docs/threejs_test.js
@@ -8,7 +8,7 @@ document.body.appendChild(cnv);
 document.body.appendChild(vid);
 
 //cnv.crossOrigin = "anonymous";
-vid.crossOrigin = "anonymous";
+//vid.crossOrigin = "anonymous";
 var ctx = null;
 
 if(true) {
@@ -38,13 +38,15 @@ if(true) {
 }
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.domElement.crossOrigin = 'anonymous';
+//renderer.domElement.crossOrigin = 'anonymous';
 document.body.appendChild(renderer.domElement);
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 var controls = new THREE.VRControls(camera);
 var geometry = new THREE.SphereBufferGeometry(100, 64, 64);
 var texture = new THREE.Texture(vid);
+texture.min_filter = THREE.LinearFilter;
+texture.mag_filter = THREE.LinearFilter;
 var material = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide});
 var mesh = new THREE.Mesh(geometry, material);
 mesh.scale.set(-1, 1, 1);
