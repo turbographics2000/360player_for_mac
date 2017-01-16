f0625315f86ae8055daefc99cf6e3ce11a766130
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-16 14:34:13 +0900

    Update same_origin.js

--- docs/same_origin.js
+++ docs/same_origin.js
@@ -10,7 +10,7 @@ vid.onloadedmetadata = function() {
 }
 
 var renderer = new THREE.WebGLRenderer();
-effect.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
