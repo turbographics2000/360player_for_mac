ddd3645fbd108181b302647f8895ecbdf4ca75f8
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-16 15:28:49 +0900

    Update m3u8.js

--- docs/m3u8.js
+++ docs/m3u8.js
@@ -1,6 +1,8 @@
 var cnv = document.createElement('canvas');
 var vid = document.createElement('video');
 var ctx = null;
+cnv.crossOrigin = 'anonymous';
+vid.crossOrigin = 'anonymous';
 
 vid.src = 'https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8';
 vid.onloadedmetadata = function() {
