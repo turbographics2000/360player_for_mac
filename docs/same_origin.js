6712d61883aa370abb3c4ed1f7827360b4b31304
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-16 15:05:39 +0900

    Update same_origin.js

--- docs/same_origin.js
+++ docs/same_origin.js
@@ -1,32 +1,29 @@
-var cnv = document.createElement('canvas');
 var vid = document.createElement('video');
-var ctx = null;
-
-vid.src = 'output.mp4';
 vid.onloadedmetadata = function() {
   ctx = cnv.getContext('2d');
   vid.play();
   render();
 }
+vid.src = 'output.mp4';
 
 var renderer = new THREE.WebGLRenderer();
-document.body.appendChild(renderer.domElement);
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 var controls = new THREE.VRControls(camera);
 var geometry = new THREE.SphereBufferGeometry(100, 64, 64);
 var texture = new THREE.Texture(vid);
-texture.min_filter = THREE.LinearFilter;
-texture.mag_filter = THREE.LinearFilter;
 var material = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide});
 var mesh = new THREE.Mesh(geometry, material);
+var effect = new THREE.VREffect(renderer);
+var manager = new WebVRManager(renderer, effect);
 mesh.scale.set(-1, 1, 1);
 scene.add(camera);
 scene.add(mesh);
-
-var effect = new THREE.VREffect(renderer);
+texture.min_filter = THREE.LinearFilter;
+texture.mag_filter = THREE.LinearFilter;
 effect.setSize(window.innerWidth, window.innerHeight);
-var manager = new WebVRManager(renderer, effect);
+
+document.body.appendChild(renderer.domElement);
 
 window.addEventListener('resize', onResize, true);
 window.addEventListener('vrdisplaypresentchange', onResize, true);
