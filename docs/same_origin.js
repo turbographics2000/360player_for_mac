6226cdb688a0caefcc7f7f5e98f8fa1bc7b7d212
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-16 14:32:29 +0900

    Update same_origin.js

--- docs/same_origin.js
+++ docs/same_origin.js
@@ -10,11 +10,9 @@ vid.onloadedmetadata = function() {
 }
 
 var renderer = new THREE.WebGLRenderer();
-document.body.appendChild(renderer.domElement);
-var effect = new THREE.VREffect(renderer);
 effect.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 var scene = new THREE.Scene();
-var manager = new THREE.WebVRManager(renderer, effect);
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 var controls = new THREE.VRControls(camera);
 var geometry = new THREE.SphereBufferGeometry(100, 64, 64);
@@ -26,11 +24,23 @@ var mesh = new THREE.Mesh(geometry, material);
 mesh.scale.set(-1, 1, 1);
 scene.add(camera);
 scene.add(mesh);
+
+var effect = new THREE.VREffect(renderer);
+effect.setSize(window.innerWidth, window.innerHeight);
+var manager = new THREE.WebVRManager(renderer, effect);
+
+window.addEventListener('resize', onResize, true);
+window.addEventListener('vrdisplaypresentchange', onResize, true);
+
+function onResize() {
+  effect.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
+
 function render() {
   requestAnimationFrame(render);
   controls.update();
   texture.needsUpdate = true;
-  if(renderer) {
-    renderer.render(scene, camera);
-  }
+  manager.render(scene, camera);
 }
