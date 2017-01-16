7dd9acd8169ffe3fa2cb602b8e4808f6d038af46
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-16 14:27:04 +0900

    Create hlsjs_m3u8.js

---
+++ docs/hlsjs_m3u8.js
@@ -0,0 +1,43 @@
+var cnv = document.createElement('canvas');
+var vid = document.createElement('video');
+var ctx = null;
+
+var hls = new Hls();
+hls.loadSource('https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8');
+vid.onloadedmetadata = function() {
+  ctx = cnv.getContext('2d');
+  vid.play();
+  render();
+}
+hls.attachMedia(vid);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+//renderer.domElement.crossOrigin = 'anonymous';
+document.body.appendChild(renderer.domElement);
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+var controls = new THREE.VRControls(camera);
+var geometry = new THREE.SphereBufferGeometry(100, 64, 64);
+//var texture = new THREE.Texture(cnv);
+var texture = new THREE.Texture(vid);
+texture.min_filter = THREE.LinearFilter;
+texture.mag_filter = THREE.LinearFilter;
+var material = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide});
+var mesh = new THREE.Mesh(geometry, material);
+mesh.scale.set(-1, 1, 1);
+scene.add(camera);
+scene.add(mesh);
+function render() {
+  requestAnimationFrame(render);
+  controls.update();
+  cnv.width = vid.videoWidth;
+  cnv.height = vid.videoHeight;
+  if(ctx && vid.videoWidth && vid.videoHeight) {
+    ctx.drawImage(vid, 0, 0, cnv.width, cnv.height);
+  }
+  texture.needsUpdate = true;
+  if(renderer) {
+    renderer.render(scene, camera);
+  }
+}
