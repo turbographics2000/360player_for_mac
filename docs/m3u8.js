4e4d6a80561bf95b7790ffd520428df9ef2772b4
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-16 14:41:13 +0900

    Update m3u8.js

--- docs/m3u8.js
+++ docs/m3u8.js
@@ -2,21 +2,19 @@ var cnv = document.createElement('canvas');
 var vid = document.createElement('video');
 var ctx = null;
 
-  vid.src = 'https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8';
+vid.src = 'https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8';
 vid.onloadedmetadata = function() {
   ctx = cnv.getContext('2d');
   vid.play();
   render();
 }
+
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-//renderer.domElement.crossOrigin = 'anonymous';
 document.body.appendChild(renderer.domElement);
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 var controls = new THREE.VRControls(camera);
 var geometry = new THREE.SphereBufferGeometry(100, 64, 64);
-//var texture = new THREE.Texture(cnv);
 var texture = new THREE.Texture(vid);
 texture.min_filter = THREE.LinearFilter;
 texture.mag_filter = THREE.LinearFilter;
@@ -25,18 +23,23 @@ var mesh = new THREE.Mesh(geometry, material);
 mesh.scale.set(-1, 1, 1);
 scene.add(camera);
 scene.add(mesh);
+
+var effect = new THREE.VREffect(renderer);
+effect.setSize(window.innerWidth, window.innerHeight);
+var manager = new WebVRManager(renderer, effect);
+
+window.addEventListener('resize', onResize, true);
+window.addEventListener('vrdisplaypresentchange', onResize, true);
+
+function onResize() {
+  effect.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
+
 function render() {
   requestAnimationFrame(render);
   controls.update();
-  /*if(!cnv.width || !cnv.height) {
-    cnv.width = vid.videoWidth;
-    cnv.height = vid.videoHeight;
-  }
-  if(ctx && vid.videoWidth && vid.videoHeight) {
-    ctx.drawImage(vid, 0, 0, cnv.width, cnv.height);
-  }*/
   texture.needsUpdate = true;
-  if(renderer) {
-    renderer.render(scene, camera);
-  }
+  manager.render(scene, camera);
 }
