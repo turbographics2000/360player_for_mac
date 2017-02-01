e92da537222333b3a7f9e05d08f9bc81cff25cc6
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-02-01 22:16:03 +0900

    Update m3u8_texturesrc_canvas.js

--- docs/m3u8_texturesrc_canvas.js
+++ docs/m3u8_texturesrc_canvas.js
@@ -3,7 +3,7 @@ var vid = document.createElement('video');
 var ctx = null;
 cnv.crossOrigin = 'anonymous';
 vid.crossOrigin = 'anonymous';
-vid.setAttribute('webkit-playsinline', true);
+vid.setAttribute('webkit-playsinline', 'webkit-playsinline');
 
 vid.src = 'https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8';
 vid.onloadedmetadata = function() {
