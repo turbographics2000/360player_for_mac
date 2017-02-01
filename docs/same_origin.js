7afc884d6ab0f5d84271de5007497db1fd174fd2
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-02-01 22:53:09 +0900

    Update same_origin.js

--- docs/same_origin.js
+++ docs/same_origin.js
@@ -4,7 +4,7 @@ vid.onloadedmetadata = function() {
   render();
 }
 vid.src = 'output.mp4';
-
+vid.setAttribute('webkit-playsinline', 'webkit-playsinline');
 var renderer = new THREE.WebGLRenderer();
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
